c5dcdd955bcc55ba39b281b883a35d3dca0941cb
cozyhorse <100480213+cozyhorse@users.noreply.github.com>
2024-09-28 12:20:06 +0200

    setup

---
+++ quiztopiaAPI/db.mjs
@@ -0,0 +1,8 @@
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
+
+
+export const client = new DynamoDBClient({
+});
+
+export const docClient = new DynamoDBDocumentClient(client);
